43212260e34b0262b377bb33ea1bb68dee08a4a7
ForgQi <34411314+ForgQi@users.noreply.github.com>
2019-05-22 11:05:50 +0800

    Initial commit

---
+++ resource-base-server/src/main/java/com/forgqi/resourcebaseserver/ResourceBaseServerApplication.java
@@ -0,0 +1,13 @@
+package com.forgqi.resourcebaseserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceBaseServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceBaseServerApplication.class, args);
+    }
+
+}
