3095afa2bb3703e67149c39630cf7daf752504e7
ForgQi <34411314+ForgQi@users.noreply.github.com>
2020-05-12 22:08:13 +0800

    Update RichTextHelper.java

--- resource-base-server/src/main/java/com/forgqi/resourcebaseserver/service/dto/util/RichTextHelper.java
+++ resource-base-server/src/main/java/com/forgqi/resourcebaseserver/service/dto/util/RichTextHelper.java
@@ -2,16 +2,17 @@ package com.forgqi.resourcebaseserver.service.dto.util;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.safety.Whitelist;
 
 public class RichTextHelper {
-    private Document document;
+    private final String html;
 
     public RichTextHelper(String html) {
-        document = Jsoup.parse(html);
+        this.html = html;
     }
 
     public String parseSummary() {
-        String s = document.text();
+        String s = Jsoup.clean(html, Whitelist.basic());
         return s.length() > 100 ? s.substring(0, 99) : s;
     }
 }
